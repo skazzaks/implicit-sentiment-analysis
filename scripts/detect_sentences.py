c51c2b87abf1ea488a8355d8be295287405ea493
Devon Fritz <devon.s.fritz@gmail.com>
2015-05-26 13:42:47 +0200

    minimum length requirement to detection phase

--- scripts/detect_sentences.py
+++ scripts/detect_sentences.py
@@ -4,6 +4,7 @@
 # Figures out the sentences from which we can extra sentiment information from
 # a list of dependency parsed sentences
 import sys
+import random
 
 POS_SENT_NUM = 0
 POS_WORD = 1
@@ -11,9 +12,10 @@ POS_POS = 5
 POS_ROLE = 11
 POS_SUB = "SB"
 POS_PPER = "PPER"
+MAX_LEN = 10
 
 input_file = sys.argv[1]
-
+random.seed()
 
 # This is our first shot at detection - grab all the sentences that
 # only have one subject
@@ -24,6 +26,8 @@ def phase1_detection(f):
     previous_sentence = ""
     total_sentences = 0
     total_successes = 0
+    current_word = 1
+    sentences = []
     
     for line in f:
         if len(line) < 2:
@@ -32,6 +36,7 @@ def phase1_detection(f):
             if success:
                 total_successes += 1
                 #print previous_sentence
+                sentences.append(previous_sentence)
 
             # 2) Reset variables
             new_sentence = True
@@ -40,6 +45,8 @@ def phase1_detection(f):
             first_sentence = False
             found_subject = False
             total_sentences += 1
+            current_word = 1
+            
         else:
             s = line.split()
 
@@ -55,9 +62,19 @@ def phase1_detection(f):
             if s[POS_POS] == POS_PPER:
                 success = False
 
-            previous_sentence += " " + s[POS_WORD]
+            # Sentence length limit
+            if current_word > MAX_LEN:
+                success = False
 
-    print "Sentence Count: " + str(total_sentences) + "\tSuccesses: " + str(total_successes)
+            previous_sentence += " " + s[POS_WORD]
+            current_word += 1
+
+    print "Sentence Count: " + str(total_sentences) + "\tSuccesses: " + str(total_successes) + "\n"
+    print "Sample Sentences: \n"
+    for i in range(1, 25):
+        x = random.randrange(0, len(sentences))
+        print sentences[x]
+    
 with open(input_file) as f:
     phase1_detection(f)
     
